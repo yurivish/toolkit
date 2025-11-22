3b718fcf2df0adacc6daf24d5a623301366b27e2
Yuri Vishnevsky <yurivish@gmail.com>
2025-11-22 00:07:20 -0500

    hot reload handler

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/yurivish/toolkit
 
 go 1.25.2
+
+require github.com/starfederation/datastar-go v1.0.3
+
+require (
+	github.com/CAFxX/httpcompression v0.0.9 // indirect
+	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+)
---
+++ handlers/hotreload.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"net/http"
+	"sync"
+
+	"github.com/starfederation/datastar-go/datastar"
+)
+
+var hotReloadOnlyOnce sync.Once
+
+// hotreloadHandler is designed for the use case of a developer with a single
+// browser tab open. That tab should have the following code:
+//
+// <div data-init="@get('/hotreload', {retryMaxCount: 1000, retryInterval: 20, retryMaxWaitMs: 200})" id="hotreload"></div>
+//
+// When the server is shut down, this will attempt to reconnect until the server is restarted.
+// That first successful reconnection will trigger the new server to send the reload script.
+// After that initial reload, this handler will simply send an empty SSE connection thanks to
+// the use of sync.Once.
+func hotreloadHandler(w http.ResponseWriter, r *http.Request) {
+	sse := datastar.NewSSE(w, r)
+	hotReloadOnlyOnce.Do(func() {
+		sse.ExecuteScript("window.location.reload()")
+	})
+	<-r.Context().Done()
+}
